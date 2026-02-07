ac5dd2b75c7bda69fcb5b73d18431d3e1d994ddf
delia <deliarata28@yahoo.com>
2026-02-07 20:36:12 +0200

    first commit (ignore)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header/Header';
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <h2 style={{ textAlign: 'center', marginTop: '50px' }}>
+                   LOVE DELIA
+                </h2>
+            </main>
+        </div>
+    );
+}
+
+export default App
